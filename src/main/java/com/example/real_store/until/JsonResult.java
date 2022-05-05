76cfc6c0adfe83c97741dd6ed73359593846f748
Chenbaoyu <1743678777@qq.com>
2022-05-05 21:29:21 +0800

    在测试类中，熟悉了mybaits-plus

--- src/main/java/com/example/real_store/until/JsonResult.java
+++ src/main/java/com/example/real_store/until/JsonResult.java
@@ -1,7 +1,32 @@
 package com.example.real_store.until;
 
-public class JsonResult {
+import java.io.Serializable;
 
+public class JsonResult<E> implements Serializable {
 
+//    状态码
+    private Integer state;
 
+    private String massage;
+
+    private E data;
+
+    public JsonResult(){
+        super();
+    }
+
+    public JsonResult(Integer state) {
+        super();
+        this.state = state;
+    }
+
+    public JsonResult(Throwable e){
+        super();
+        this.massage=e.getMessage();
+    }
+
+    public JsonResult(Integer state, E data) {
+        this.state = state;
+        this.data = data;
+    }
 }
