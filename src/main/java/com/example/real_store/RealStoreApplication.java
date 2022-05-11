a5e04db798130f342a58ffcc5ad2f2d95e84145b
Chenbaoyu <1743678777@qq.com>
2022-05-11 23:05:55 +0800

    upload.html中js编写，了解了上传图片

--- src/main/java/com/example/real_store/RealStoreApplication.java
+++ src/main/java/com/example/real_store/RealStoreApplication.java
@@ -3,7 +3,16 @@ package com.example.real_store;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
 
+import javax.servlet.MultipartConfigElement;
+
+
+@Configuration
 @MapperScan("com.example.real_store.mapper")
 @SpringBootApplication
 public class RealStoreApplication {
@@ -11,5 +20,20 @@ public class RealStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(RealStoreApplication.class, args);
     }
+
+    @Bean
+    public MultipartConfigElement getMulipartConfigElement(){
+        MultipartConfigFactory multipartConfigFactory=new MultipartConfigFactory();
+
+        DataSize dataSize=DataSize.ofMegabytes(10);
+
+        multipartConfigFactory.setMaxFileSize(DataSize.of(10, DataUnit.MEGABYTES));
+        multipartConfigFactory.setMaxRequestSize(DataSize.of(10,DataUnit.MEGABYTES));
+
+        return multipartConfigFactory.createMultipartConfig();
+
+    }
+
+
 
 }
